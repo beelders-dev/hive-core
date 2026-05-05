6209f36393f83b0efd177512b804ac8223eb44e2
beelders-studio <mickeyuy.dev@gmail.com>
2026-05-05 11:06:51 +0800

    add test to inventory

--- inventory/tests.py
+++ inventory/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.urls import reverse
+
+from .models import Ingredient
 
 # Create your tests here.
+
+
+class IngredientTests(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.ingredient = Ingredient.objects.create(
+            name="egg", stock_qty=12.00, price=25.00
+        )
+
+    # Model tests
+    def test_ingredient_content(self):
+        """Test that model fields save correctly"""
+        self.assertEqual(self.ingredient.name, "egg")
+        self.assertEqual(self.ingredient.stock_qty, 12.00)
+        self.assertEqual(self.ingredient.price, 25.00)
+
+    # View tests
+    def test_ingredient_list_view(self):
+        response = self.client.get(reverse("inventory:ingredient_list"))
+        self.assertEqual(response.status_code, 200)
--- production/urls.py
+++ production/urls.py
@@ -6,6 +6,6 @@ app_name = "production"
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="temp.html"), name="recipe_list"),
-    path("recipe/", RecipeListView.as_view(), name="recipe_list"),
-    path("recipe/<uuid:pk>/", RecipeDetailView.as_view(), name="recipe_detail"),
+    path("recipes/", RecipeListView.as_view(), name="recipe_list"),
+    path("recipes/<uuid:pk>/", RecipeDetailView.as_view(), name="recipe_detail"),
 ]
