4201da8940f55c0adb7fc45c8514ba6a07a6282e
beelders-studio <mickeyuy.dev@gmail.com>
2026-05-08 02:25:30 +0800

    add delete, detail. fix UI

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 
-from .views import IngredientListView, IngredientCreateView
+from .views import (
+    IngredientListView,
+    IngredientCreateView,
+    IngredientDetailView,
+    IngredientDeleteView,
+)
 
 app_name = "inventory"
 
 urlpatterns = [
     path("", IngredientListView.as_view(), name="ingredient_list"),
-    path("add/", IngredientCreateView.as_view(), name="add_ingredient"),
+    path("add/", IngredientCreateView.as_view(), name="ingredient_add"),
+    path("<uuid:pk>/", IngredientDetailView.as_view(), name="ingredient"),
+    path("<uuid:pk>/delete/", IngredientDeleteView.as_view(), name="ingredient_delete"),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DeleteView, DetailView
+from django.urls import reverse_lazy
 
 from .models import Ingredient
 
@@ -14,3 +15,16 @@ class IngredientListView(ListView):
 class IngredientCreateView(CreateView):
     model = Ingredient
     template_name = "inventory/ingredient_add.html"
+    fields = ["name", "stock_qty", "price"]
+    success_url = reverse_lazy("inventory:ingredient_list")
+
+
+class IngredientDeleteView(DeleteView):
+    model = Ingredient
+    template_name = "inventory/ingredient_delete.html"
+    success_url = reverse_lazy("inventory:ingredient_list")
+
+
+class IngredientDetailView(DetailView):
+    model = Ingredient
+    template_name = "inventory/ingredient_detail.html"
