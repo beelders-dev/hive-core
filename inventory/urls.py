ebc280c657b46132253d2a5edf925e1e1e9d1ec2
beelders-studio <mickeyuy.dev@gmail.com>
2026-05-07 09:17:55 +0800

    fix tailwind

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import IngredientListView
+from .views import IngredientListView, IngredientCreateView
 
 app_name = "inventory"
 
 urlpatterns = [
     path("", IngredientListView.as_view(), name="ingredient_list"),
+    path("add/", IngredientCreateView.as_view(), name="add_ingredient"),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 
 from .models import Ingredient
 
@@ -9,3 +9,8 @@ class IngredientListView(ListView):
     model = Ingredient
     template_name = "inventory/ingredient_list.html"
     context_object_name = "ingredient_list"
+
+
+class IngredientCreateView(CreateView):
+    model = Ingredient
+    template_name = "inventory/ingredient_add.html"
