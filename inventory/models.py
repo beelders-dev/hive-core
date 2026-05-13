f3320937ecf969afbd535195eb1732d00d43d1b3
beelders-studio <mickeyuy.dev@gmail.com>
2026-05-13 08:18:23 +0800

    create model tests

--- inventory/models.py
+++ inventory/models.py
@@ -1,4 +1,5 @@
 import uuid
+from django.core.validators import MinValueValidator
 from django.db import models
 from django.urls import reverse
 
@@ -8,11 +9,18 @@ from django.urls import reverse
 class Ingredient(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
-    stock_qty = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-    price = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
+    stock_qty = models.DecimalField(
+        max_digits=10, decimal_places=2, default=0.00, validators=[MinValueValidator(0)]
+    )
+    price = models.DecimalField(
+        max_digits=6, decimal_places=2, default=1.00, validators=[MinValueValidator(1)]
+    )
+
+    def get_total_amount(self):
+        return self.stock_qty * self.price
 
     def __str__(self):
-        return f"{self.name}  ({self.stock_qty}g)"
+        return f"{self.name}  ({self.stock_qty:.2f}g)"
 
     def get_absolute_url(self):
         return reverse("inventory:ingredient", kwargs={"pk": self.pk})
--- inventory/tests.py
+++ inventory/tests.py
@@ -1,3 +1,5 @@
+from django.core.exceptions import ValidationError
+from decimal import Decimal
 from django.test import TestCase
 from django.urls import reverse
 
@@ -16,10 +18,40 @@ class IngredientTests(TestCase):
 
     # Model tests
     def test_ingredient_content(self):
-        """Test that model fields save correctly"""
         self.assertEqual(self.ingredient.name, "egg")
-        self.assertEqual(self.ingredient.stock_qty, 12.00)
-        self.assertEqual(self.ingredient.price, 25.00)
+        self.assertEqual(self.ingredient.stock_qty, Decimal(12.00))
+        self.assertEqual(self.ingredient.price, Decimal(25.00))
+
+    def test_ingredient_str(self):
+        expected = "egg  (12.00g)"
+        self.assertEqual(str(self.ingredient), expected)
+
+    def test_get_absolute_url(self):
+        self.assertEqual(
+            self.ingredient.get_absolute_url(),
+            reverse("inventory:ingredient", kwargs={"pk": self.ingredient.pk}),
+        )
+
+    def test_get_total_amount(self):
+        self.assertEqual(self.ingredient.get_total_amount(), Decimal(300.00))
+
+    def test_stock_cannot_be_negative(self):
+
+        invalid_ingredient = Ingredient.objects.create(
+            name="apple", stock_qty=-20.00, price=25.00
+        )
+
+        with self.assertRaises(ValidationError):
+            invalid_ingredient.full_clean()
+
+    def test_price_cannot_be_negative(self):
+
+        invalid_ingredient = Ingredient.objects.create(
+            name="apple", stock_qty=20.00, price=-25.00
+        )
+
+        with self.assertRaises(ValidationError):
+            invalid_ingredient.full_clean()
 
     # View tests
     def test_ingredient_list_view(self):
