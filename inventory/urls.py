42acbd4d5c3c0ae7335a89f4cf4ffe3a41349bf7
beelders-studio <mickeyuy.dev@gmail.com>
2026-06-29 15:36:50 +0800

    add ingredients in url for inventory app

--- inventory/urls.py
+++ inventory/urls.py
@@ -13,18 +13,24 @@ from .views import (
 app_name = "inventory"
 
 urlpatterns = [
-    path("", IngredientListView.as_view(), name="ingredient_list"),
-    path("create/", IngredientCreateView.as_view(), name="ingredient_create"),
-    path("<uuid:pk>/", IngredientDetailView.as_view(), name="ingredient"),
-    path("<uuid:pk>/delete/", IngredientDeleteView.as_view(), name="ingredient_delete"),
+    path("ingredients/", IngredientListView.as_view(), name="ingredient_list"),
+    path(
+        "ingredients/create/", IngredientCreateView.as_view(), name="ingredient_create"
+    ),
+    path("ingredients/<uuid:pk>/", IngredientDetailView.as_view(), name="ingredient"),
+    path(
+        "ingredients/<uuid:pk>/delete/",
+        IngredientDeleteView.as_view(),
+        name="ingredient_delete",
+    ),
     # path("<uuid:pk>/edit/", IngredientUpdateView.as_view(), name="ingredient_update"),
     path(
-        "<uuid:pk>/ingredient-purchase-list/",
+        "ingredients/<uuid:pk>/purchase-list/",
         IngredientPurchaseListView.as_view(),
         name="ingredient_purchase_list",
     ),
     path(
-        "<uuid:pk>/ingredient-purchase-create/",
+        "ingredients/<uuid:pk>/purchase-create/",
         IngredientPurchaseCreateView.as_view(),
         name="ingredient_purchase_create",
     ),
