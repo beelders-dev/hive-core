3574a4e4b471ec924af55bc36fc49d736affa1cb
beelders-studio <mickeyuy.dev@gmail.com>
2026-07-01 01:47:23 +0800

    delete home - replace with dashboard

--- pages/views.py
+++ pages/views.py
@@ -10,7 +10,7 @@ from production.models import Recipe
 
 
 class HomePageView(LoginRequiredMixin, TemplateView):
-    template_name = "home.html"
+    template_name = "dashboard.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
