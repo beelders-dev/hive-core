a822b3091f5f1e82fcddb18821be03756a5cbd8e
beelders-studio <mickeyuy.dev@gmail.com>
2026-08-08 13:50:50 +0800

    Fix production environment settings

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ load_dotenv(BASE_DIR / ".env")
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
 
 ALLOWED_HOSTS = [host for host in os.getenv("ALLOWED_HOSTS", "").split(",") if host]
